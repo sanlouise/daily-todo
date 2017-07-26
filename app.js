46bf26e5e2e9075015928c0c8d9e403f9fbbd5bf
sanlouise <sandra.adamshallie@gmail.com>
2017-07-26 14:36:07 -0400

    Add ids to todo items

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
 
 let isDone;
-let existingTodos;
+let existingTodos = JSON.parse(fs.readFileSync('./existingtodos.json'));
+let todoId;
 
-const addTodo = (body, isDone = false) => {
-  let todo = { body, isDone };
-
-  existingTodos = JSON.parse(fs.readFileSync('./existingtodos.json'));
+const addTodo = (body, isDone = false, todoId) => {
+  todoId = existingTodos.todos.length + 1;
+  let todo = { body, isDone, todoId };
 
   existingTodos.todos.push(todo);
   fs.writeFileSync('./existingtodos.json', JSON.stringify(existingTodos));
   console.log({existingTodos})
-
 };
 
-module.exports = { addTodo, existingTodos };
+module.exports = { addTodo, existingTodos};
